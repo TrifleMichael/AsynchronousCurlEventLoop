7eccb1538616c39e29ec032a49d7a82d7af1fc5f
Michal <trzebuniak.michal@gmail.com>
2022-08-05 10:51:35 +0200

    Refactor

--- AsynchronousDownloader.h
+++ AsynchronousDownloader.h
@@ -12,7 +12,7 @@
 #include <vector>
 #include <unordered_map>
 
-#include <chrono> // time measurement
+#include <chrono>   // time measurement
 #include <unistd.h> // time measurement
 
 #ifndef ASYNCHRONOUSDOWNLOADER_H_
@@ -25,69 +25,67 @@ void curl_perform(uv_poll_t *req, int status, int events);
 
 class AsynchronousDownloader
 {
-  public:
-    uint64_t curlBuffer = 950; // miliseconds durning which handle will be left open after last call
-    int loopIterations = 5000;
+public:
+  uint64_t curlBuffer = 800; // miliseconds durning which handle will be left open after last call
+  int loopIterations = 5000;
 
-    bool closeLoop = false;
-    uv_loop_t *loop = nullptr;
-    CURLM *curl_handle = nullptr;
-    uv_timer_t *timeout;
+  bool closeLoop = false;
+  uv_loop_t *loop = nullptr;
+  CURLM *curlMultiHandle = nullptr;
+  uv_timer_t *timeout;
 
-typedef struct curl_context_s
-{
-  uv_poll_t poll_handle;
-  curl_socket_t sockfd;
-  AsynchronousDownloader* objPtr = nullptr;
-} curl_context_t;
+  typedef struct curl_context_s
+  {
+    uv_poll_t poll_handle;
+    curl_socket_t sockfd;
+    AsynchronousDownloader *objPtr = nullptr;
+  } curl_context_t;
 
-typedef struct CurlHandleData
-{
-  bool inUse = true;
-  int index;
-  CURL* curlHandle;
-  uv_timer_t timerHandle;
-  int queueIndex;
-} CurlHandleData;
+  typedef struct CurlHandleData
+  {
+    bool inUse = true;
+    int index;
+    CURL *curlHandle;
+    uv_timer_t timerHandle;
+    int queueIndex;
+  } CurlHandleData;
 
-typedef struct UvTimerHandleData
-{
-  bool refresh = true;
-  CurlHandleData *curlHandleData = nullptr;
-  AsynchronousDownloader *downloaderPtr;
-} UvTimerHandleData;
+  typedef struct UvTimerHandleData
+  {
+    bool refresh = true;
+    CurlHandleData *curlHandleData = nullptr;
+    AsynchronousDownloader *downloaderPtr;
+  } UvTimerHandleData;
 
-typedef struct DataForSocket
-{
-  AsynchronousDownloader *objPtr;
-  CURLM *curlm;
-} DataForSocket;
+  typedef struct DataForSocket
+  {
+    AsynchronousDownloader *objPtr;
+    CURLM *curlm;
+  } DataForSocket;
 
-std::vector<std::unordered_map<std::string, std::string*>*> urlContentMapQueue;
-std::vector<int> queueStatus;
-std::vector<int> queueProgress;
-std::unordered_map<int, CurlHandleData*> handleDataMap;
+  std::vector<std::unordered_map<std::string, std::string *> *> urlContentMapQueue;
+  std::vector<int> queueStatus;
+  std::vector<int> queueProgress;
+  std::unordered_map<int, CurlHandleData *> handleDataMap;
 
-static curl_context_t* create_curl_context(curl_socket_t sockfd, AsynchronousDownloader* objPtr);
-static void curl_close_cb(uv_handle_t *handle);
-static void destroy_curl_context(curl_context_t *context);
-static int createCurlHandleIndex(std::unordered_map<int, AsynchronousDownloader::CurlHandleData *> *handleDataMap);
-static CurlHandleData* createCurlHandleData(CURL *handle, int queueIndex, std::unordered_map<int, AsynchronousDownloader::CurlHandleData *> *handleDataMap, uv_loop_t *loop);
-static size_t myCallback(void *contents, size_t size, size_t nmemb, std::string *dst);
-static void startDownload(std::string *dst, std::string url, int queueInd, std::unordered_map<int, CurlHandleData*> *handleDataMap, uv_loop_t *loop, CURLM *curl_handle);
-void check_multi_info(void);
-static int start_timeout(CURLM *multi, long timeout_ms, void *userp);
-static int handle_socket(CURL *easy, curl_socket_t s, int action, void *userp, void *socketp);
-void runDownloadsFromMap(std::unordered_map<std::string, std::string*> *urlContentMap, int queueIndex);
-std::unordered_map<std::string, std::string*>* urlVectorToUrlContentMap(std::vector<std::string> urlVector);
-void printBar();
-void printContents(std::unordered_map<std::string, std::string*> *urlContentMap);
-int oldMain();
-int addDownloadTask(std::vector<std::string> urlVector);
-std::unordered_map<std::string, std::string*>* getResponse(int index);
-std::string* getResponse(int index, std::string url);
-
-
+  static curl_context_t *createCurlContext(curl_socket_t sockfd, AsynchronousDownloader *objPtr);
+  static void curlCloseCB(uv_handle_t *handle);
+  static void destroyCurlContext(curl_context_t *context);
+  static int createCurlHandleIndex(std::unordered_map<int, AsynchronousDownloader::CurlHandleData *> *handleDataMap);
+  static CurlHandleData *createCurlHandleData(CURL *handle, int queueIndex, std::unordered_map<int, AsynchronousDownloader::CurlHandleData *> *handleDataMap, uv_loop_t *loop);
+  static size_t myCallback(void *contents, size_t size, size_t nmemb, std::string *dst);
+  static void startDownload(std::string *dst, std::string url, int queueInd, std::unordered_map<int, CurlHandleData *> *handleDataMap, uv_loop_t *loop, CURLM *curlMultiHandle);
+  void checkMultiInfo(void);
+  static int startTimeout(CURLM *multi, long timeout_ms, void *userp);
+  static int handleSocket(CURL *easy, curl_socket_t s, int action, void *userp, void *socketp);
+  void runDownloadsFromMap(std::unordered_map<std::string, std::string *> *urlContentMap, int queueIndex);
+  std::unordered_map<std::string, std::string *> *urlVectorToUrlContentMap(std::vector<std::string> urlVector);
+  void printBar();
+  void printContents(std::unordered_map<std::string, std::string *> *urlContentMap);
+  int oldMain();
+  int addDownloadTask(std::vector<std::string> urlVector);
+  std::unordered_map<std::string, std::string *> *getResponse(int index);
+  std::string *getResponse(int index, std::string url);
 };
 
 #endif
